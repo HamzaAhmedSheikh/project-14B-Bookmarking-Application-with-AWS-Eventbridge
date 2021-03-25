e176a244a436b0e021b706040f8b4aae82b62d9b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2021-03-25 23:14:21 +0500

    I have set up basic structure of aws-cdk.

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,4 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as appsync from '@aws-cdk/aws-appsync';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
+
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
